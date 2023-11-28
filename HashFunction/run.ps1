a82555f5d057a3ada20ebab48e0e17afa054fe18
Jack Musick <jack@gocovi.com>
2023-11-27 23:15:59 -0500

    Initial commit

---
+++ HashFunction/run.ps1
@@ -0,0 +1,27 @@
+using namespace System.Net
+
+# Input bindings are passed in via param block.
+param($Request, $TriggerMetadata)
+
+if (!$Request.Query.algorithm -or !$Request.Query.content) {
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::BadRequest;
+            Headers    = @{
+                "Content-type" = "application/json"
+            };
+            Body       = @{ error = "Missing Algorithm and or Content" } | ConvertTo-Json;
+        })
+}
+else {
+    $Hash = [System.Security.Cryptography.HashAlgorithm]::Create($Request.Query.algorithm.ToLower()).ComputeHash([System.Text.Encoding]::UTF8.GetBytes($Request.Query.content))
+    
+    $Result = ([System.BitConverter]::ToString($Hash).Replace('-', '').ToLower())
+
+    Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
+            StatusCode = [HttpStatusCode]::BadRequest;
+            Headers    = @{
+                "Content-type" = "application/json"
+            };
+            Body       = @{ result = $Result } | ConvertTo-Json;
+        })
+}
