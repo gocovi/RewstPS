9291c7da47334086f0aae04ec58e35ae68984afa
Jack Musick <jack@gocovi.com>
2024-04-17 10:21:19 -0400

    Input support
    The body of a request now supports JSON with an input and a script property.

--- ScriptRunner/run.ps1
+++ ScriptRunner/run.ps1
@@ -4,11 +4,30 @@ using namespace System.Net
 param($Request, $TriggerMetadata)
 
 try {
-    if ($Request.Query.encoded -eq "true") {
-        $ScriptContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($Request.Body))
+    if ($null -eq $Request.Body.script) {
+        Write-Host "No script provided so will use the entire request body as the script".
+        $ScriptBody = $Request.Body
     }
     else {
-        $ScriptContent = $Request.Body
+        Write-Host "Script was provided in the script property.".
+
+        $ScriptBody = $Request.Body.script
+
+        if ($Request.Body.input) {
+            Write-Host "Input was provided so will convert the input object to variables".
+            
+            # Convert all of the properites in the input object to variables
+            $Request.Body.input.GetEnumerator() | ForEach-Object {
+                Set-Variable -Name $_.key -Value $_.value
+            }
+        }
+    }
+
+    if ($Request.Query.encoded -eq "true") {
+        $ScriptContent = [System.Text.Encoding]::UTF8.GetString([System.Convert]::FromBase64String($ScriptBody))
+    }
+    else {
+        $ScriptContent = $ScriptBody
     }
 
     $Result = $ScriptContent | Invoke-Expression
