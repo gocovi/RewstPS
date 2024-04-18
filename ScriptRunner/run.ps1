b61ab545f6c60955bf117b93b95a2e587d8969d4
Jack Musick <jack@gocovi.com>
2024-04-18 15:12:37 -0400

    Dynamic Parameters
    Support has been added for param blocks in scripts to make them even more universal. Instead of simply injecting Input properties as variables, we will now test your script for any parameters requested in the param block. If any are found, we will inject those with matching variables that you supplied.

--- ScriptRunner/run.ps1
+++ ScriptRunner/run.ps1
@@ -3,24 +3,22 @@ using namespace System.Net
 # Input bindings are passed in via param block.
 param($Request, $TriggerMetadata)
 
+if (-not (Test-Path -Path "~/tmp")) {
+    New-Item -Path "~/tmp" -ItemType Directory | Out-Null
+}
+
+$ScriptGuid = [Guid]::NewGuid().ToString()
+$ScriptFilePath = "~/tmp/$ScriptGuid.ps1"
+
 try {
     if ($null -eq $Request.Body.script) {
-        Write-Host "No script provided so will use the entire request body as the script".
+        Write-Host "No script provided so will use the entire request body as the script"
         $ScriptBody = $Request.Body
     }
     else {
-        Write-Host "Script was provided in the script property.".
+        Write-Host "Script was provided in the script property"
 
         $ScriptBody = $Request.Body.script
-
-        if ($Request.Body.input) {
-            Write-Host "Input was provided so will convert the input object to variables".
-            
-            # Convert all of the properites in the input object to variables
-            $Request.Body.input.GetEnumerator() | ForEach-Object {
-                Set-Variable -Name $_.key -Value $_.value
-            }
-        }
     }
 
     if ($Request.Query.encoded -eq "true") {
@@ -30,7 +28,36 @@ try {
         $ScriptContent = $ScriptBody
     }
 
-    $Result = $ScriptContent | Invoke-Expression
+    # Used to get parameters from script
+    $ScriptWrapper = @"
+function Test-Script {
+    $ScriptContent
+}
+
+(Get-Command Test-Script).Parameters
+"@
+
+    $InputParameters = @{}
+
+    if ($Request.Body.input) {
+        Write-Host "Input was provided so will convert the input object to variables"
+            
+        # Convert all of the properites in the input object to variables
+        $Request.Body.input.GetEnumerator() | ForEach-Object {
+            Set-Variable -Name $_.key -Value $_.value
+        }
+
+        # Setting input parameters from
+        (Invoke-Expression -Command $ScriptWrapper).GetEnumerator() | ForEach-Object {
+            if ($Request.Body.input.ContainsKey($_.Key)) {
+                $InputParameters.Add($_.Key, $Request.Body.input[$_.Key])
+            }
+        }
+    }
+
+    # Save script to a temporary file
+    $ScriptContent | Out-File -FilePath $ScriptFilePath
+    $Result = . $ScriptFilePath @InputParameters
 
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
             StatusCode = [HttpStatusCode]::OK;
@@ -41,6 +68,10 @@ try {
         })
 }
 catch {
+    if ((Test-Path -Path $ScriptFilePath)) {
+        Remove-Item -Path $ScriptFilePath
+    }
+
     Write-Host $_.Exception.Message
     Push-OutputBinding -Name Response -Value ([HttpResponseContext]@{
             StatusCode = [HttpStatusCode]::BadRequest;
